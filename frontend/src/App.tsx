ae36f1f7f53134a9fa7432ac9f26f44f8edeeef5
rdum464 <rushaandumasia@gmail.com>
2021-07-31 10:37:12 +1200

    added base front end project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Button color="primary">HELLO WORLD</Button>
+    </div>
+  );
+}
+
+export default App;
